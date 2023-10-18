8cf469eb9568ec509cf3fb466209500841b0097f
yuler <is.yuler@gmail.com>
2023-10-19 00:34:16 +0800

    wip: splitpanes layout. [skip ci]

--- components/TabsApiRequest.vue
+++ components/TabsApiRequest.vue
@@ -2,10 +2,12 @@
 import type { TabItem } from '@nuxt/ui/dist/runtime/types';
 
 const tabItems: TabItem[] = [
-  { label: 'Params', slot: 'params', title: 'Query Parameters' },
-  { label: 'Body', slot: 'body', title: 'Body' },
-  { label: 'Headers', slot: 'headers', title: 'Headers' },
-  { label: 'Auth', slot: 'auth', title: 'Authorization' },
+  { label: 'Params', slot: 'params' },
+  { label: 'Body', slot: 'body' },
+  { label: 'Headers', slot: 'headers' },
+  { label: 'Auth', slot: 'auth' },
+  { label: 'Pre Request Script', slot: 'preRequestScript' },
+  { label: 'Post Response Script', slot: 'postResponseScript' },
 ]
 
 const paramsRows = [
@@ -16,7 +18,25 @@ const paramsRows = [
 <template>
   <UTabs :items="tabItems" class="w-full">
     <template #params="{ item }">
-      <p>{{ item.title }}</p>
+      <p>Query Parameters</p>
+      <UTable :rows="paramsRows" :ui="{ td: { padding: 'px-1' } }">
+        <template #key-data="{ row }">
+          <UInput v-model="row.key" placeholder="Key" />
+        </template>
+        <template #value-data="{ row }">
+          <UInput v-model="row.value" placeholder="Value" />
+        </template>
+        <template #type-data="{ row }">
+          <UInput v-model="row.type" placeholder="Type" />
+        </template>
+        <template #example-data="{ row }">
+          <UInput v-model="row.example" placeholder="Example" />
+        </template>
+        <template #description-data="{ row }">
+          <UInput v-model="row.description" placeholder="Description" />
+        </template>
+      </UTable>
+      <p>Path Parameters</p>
       <UTable :rows="paramsRows" :ui="{ td: { padding: 'px-1' } }">
         <template #key-data="{ row }">
           <UInput v-model="row.key" placeholder="Key" />
--- pages/p/[id]/index.vue
+++ pages/p/[id]/index.vue
@@ -109,8 +109,8 @@ async function onCollectionDelete(id: string) {
 </script>
 
 <template>
-  <div class="w-screen h-screen flex gap-2">
-    <div class="w-1/4 px-4 py-2 space-y-2 border">
+  <Splitpanes class="w-screen h-screen flex gap-2">
+    <Pane max-size="20" class="w-1/4 px-4 py-2 space-y-2 border">
       <UButton @click="openModalCollectionCreate">
         Create Collection
       </UButton>
@@ -144,12 +144,12 @@ async function onCollectionDelete(id: string) {
           <p>Endpoint: {{ api.name || api.endpoint }}</p>
         </UCard>
       </div>
-    </div>
+    </Pane>
 
-    <div class="w-3/4 p-1 border flex flex-col gap-2">
+    <Pane class="w-3/4 p-1 border flex flex-col gap-2">
       <div class="w-full flex gap-1">
         <USelectMenu v-model="method" :options="options" />
-        <UInput v-model="url" class="w-80" placeholder="https://echo.hoppscotch.io" />
+        <UInput v-model="url" class="w-full" placeholder="https://echo.hoppscotch.io" />
         <UButton :loading="sending" @click="onSend">
           Send
         </UButton>
@@ -157,11 +157,11 @@ async function onCollectionDelete(id: string) {
           Save
         </UButton>
       </div>
-      <Splitpanes class="w-full h-full flex gap-2">
-        <Pane min-size="25">
+      <Splitpanes class="w-full h-full flex gap-2" horizontal>
+        <Pane>
           <TabsApiRequest />
         </Pane>
-        <Pane min-size="25">
+        <Pane>
           <div class="flex p-1">
             <div class="w-32">
               Request
@@ -173,14 +173,6 @@ async function onCollectionDelete(id: string) {
           <pre>{{ response }}</pre>
         </Pane>
       </Splitpanes>
-    </div>
-  </div>
+    </Pane>
+  </Splitpanes>
 </template>
-
-<style>
-.splitpanes__splitter {
-  @apply bg-gray-200;
-  @apply transition-all;
-  @apply hover:opacity-100 hover:w-1;
-}
-</style>
